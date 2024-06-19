034f68fa0f97f9af6b8a291f2d9e1eb2bdce1a83
ShantanuKudva <kudvashantanu2002@gmail.com>
2024-06-19 11:03:11 +0530

    vanilla js is breaking. everythin else is working

--- index.js
+++ index.js
@@ -36,6 +36,295 @@ function setupGracefulExit() {
   });
 }
 
+function installTailwind(framework, projectPath, projectName, spin) {
+  exec(
+    "npm install -D tailwindcss postcss autoprefixer",
+    { cwd: projectPath },
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(chalk.red(`Failed to install Tailwind CSS: ${stderr}`));
+        process.exit(1);
+      } else {
+        console.log(chalk.bgBlue("running"), chalk.gray(stdout));
+
+        exec(
+          "npx tailwindcss init -p",
+          { cwd: projectPath },
+          (error, stdout, stderr) => {
+            if (error) {
+              console.error(
+                chalk.red(`Failed to initialize Tailwind CSS: ${stderr}`)
+              );
+              process.exit(1);
+            } else {
+              console.log(chalk.bgBlue("running"), chalk.gray(stdout));
+
+              const tailwindConfig = `
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx,ts,tsx,vue,svelte}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+          `;
+              fs.writeFileSync(
+                path.join(projectPath, "tailwind.config.js"),
+                tailwindConfig
+              );
+
+              const tailwindCSS = `
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+          `;
+              fs.writeFileSync(
+                path.join(projectPath, "src", "style.css"),
+                tailwindCSS
+              );
+
+              const mainFile = `main.${framework.includes("ts") ? "ts" : "js"}`;
+              const mainFilePath = path.join(projectPath, "src", mainFile);
+
+              if (fs.existsSync(mainFilePath)) {
+                let mainJS = fs.readFileSync(mainFilePath, "utf8");
+                mainJS = mainJS.replace(
+                  /import ['"].\/style.css['"];/,
+                  `import './style.css';`
+                );
+                fs.writeFileSync(mainFilePath, mainJS);
+              }
+
+              console.log(
+                chalk.green(
+                  `Tailwind CSS installed and configured successfully for ${framework}.`
+                )
+              );
+              spin.stop(
+                chalk.bgGreenBright("Project created successfully! 🚀")
+              );
+              process.exit(0);
+            }
+          }
+        );
+      }
+    }
+  );
+}
+
+const stubFunctions = {
+  react: (projectPath, projectName, isTs) => {
+    const extension = isTs ? "tsx" : "jsx";
+    const code = `
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Assuming your Tailwind CSS is imported here
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="text-center">
+      <p className="text-xl font-bold">Count: {count}</p>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold mb-4">Welcome to Your React App</h1>
+      <Counter />
+      <footer className="absolute bottom-0 mb-4">
+        <a href="https://github.com/ShantanuKudva/ShantanuKudva" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">GitHub</a>
+      </footer>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+`;
+    fs.writeFileSync(path.join(projectPath, "src", `main.${extension}`), code);
+  },
+
+  vue: (projectPath, projectName, isTs) => {
+    const scriptType = isTs ? 'lang="ts"' : "";
+    const appCode = `
+<template>
+  <div id="app" class="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
+    <h1 class="text-3xl font-bold mb-4">Welcome to Your Vue App</h1>
+    <Counter />
+    <footer class="absolute bottom-0 mb-4">
+      <a href="https://github.com/ShantanuKudva/ShantanuKudva" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">GitHub</a>
+    </footer>
+  </div>
+</template>
+
+<script ${scriptType}>
+import Counter from './Counter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+@import './style.css';
+</style>
+`;
+
+    const counterCode = `
+<template>
+  <div>
+    <p class="text-xl font-bold">Count: {{ count }}</p>
+    <button @click="increment" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Increment</button>
+  </div>
+</template>
+
+<script setup ${scriptType}>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+</script>
+
+<style scoped>
+/* Add your scoped styles here */
+</style>
+`;
+    fs.writeFileSync(path.join(projectPath, "src", "App.vue"), appCode);
+    fs.writeFileSync(path.join(projectPath, "src", "Counter.vue"), counterCode);
+  },
+
+  svelte: (projectPath, projectName, isTs) => {
+    const code = `
+<script ${isTs ? 'lang="ts"' : ""}>
+  let count = 0;
+  function increment() {
+    count += 1;
+  }
+</script>
+
+<main class="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
+  <h1 class="text-3xl font-bold mb-4">Welcome to Your Svelte App</h1>
+  <div>
+    <p class="text-xl font-bold">Count: {count}</p>
+    <button on:click={increment} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Increment</button>
+  </div>
+  <footer class="absolute bottom-0 mb-4">
+    <a href="https://github.com/ShantanuKudva/ShantanuKudva" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">GitHub</a>
+  </footer>
+</main>
+
+<style>
+  /* Add your styles here */
+</style>
+`;
+    fs.writeFileSync(path.join(projectPath, "src", "App.svelte"), code);
+  },
+
+  solid: (projectPath, projectName, isTs) => {
+    const extension = isTs ? "tsx" : "jsx";
+    const code = `
+import { createSignal } from "solid-js";
+
+function Counter() {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <div class="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
+      <p class="text-xl font-bold">Count: {count()}</p>
+      <button onClick={() => setCount(count() + 1)} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Increment</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div class="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
+      <h1 class="text-3xl font-bold mb-4">Welcome to Your Solid App</h1>
+      <Counter />
+      <footer class="absolute bottom-0 mb-4">
+        <a href="https://github.com/ShantanuKudva/ShantanuKudva" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">GitHub</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+`;
+    fs.writeFileSync(path.join(projectPath, "src", `main.${extension}`), code);
+  },
+
+  vanilla: (projectPath, projectName, isTs) => {
+    const jsExtension = isTs ? "ts" : "js";
+    const htmlCode = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Vanilla JS App</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body class="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
+  <div id="app">
+    <h1 class="text-3xl font-bold mb-4">Welcome to Your Vanilla JS App</h1>
+    <div id="counter">
+      <p class="text-xl font-bold">Count: <span id="count">0</span></p>
+      <button id="incrementBtn" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Increment</button>
+    </div>
+    <footer class="absolute bottom-0 mb-4">
+      <a href="https://github.com/ShantanuKudva/ShantanuKudva" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">GitHub</a>
+    </footer>
+  </div>
+  <script src="./main.${jsExtension}"></script>
+</body>
+</html>
+`;
+
+    const jsCode = `
+document.addEventListener('DOMContentLoaded', () => {
+  const countEl = document.getElementById('count');
+  const incrementBtn = document.getElementById('incrementBtn');
+  let count = 0;
+
+  incrementBtn.addEventListener('click', () => {
+    count++;
+    countEl.textContent = count;
+  });
+});
+`;
+
+    fs.writeFileSync(path.join(projectPath, "index.html"), htmlCode);
+    fs.writeFileSync(path.join(projectPath, `main.${jsExtension}`), jsCode);
+  },
+};
+function addStubCode(projectPath, framework, projectName) {
+  const isTs = framework.endsWith("-ts");
+  const baseFramework = isTs ? framework.slice(0, -3) : framework;
+  // console.log("🚀 ~ addStubCode ~ baseFramework:", baseFramework);
+
+  if (stubFunctions[baseFramework]) {
+    stubFunctions[baseFramework](projectPath, projectName, isTs);
+    console.log(`${baseFramework} stub code added successfully.`);
+  } else {
+    console.error(`No stub code available for the ${baseFramework} framework.`);
+  }
+}
+
 function displayHeader() {
   const animation = chalkAnimation.neon(
     figlet.textSync(`ViteTail Starter`, {
@@ -59,20 +348,20 @@ async function main() {
     { value: "react", label: `${emoji.get("large_blue_diamond")} React` },
     { value: "preact", label: `${emoji.get("leaves")} Preact` },
     { value: "lit", label: `${emoji.get("star2")} Lit` },
-    // { value: "svelte", label: `${emoji.get("orange_heart")} Svelte` },
     { value: "solid", label: `${emoji.get("rocket")} Solid` },
+    // { value: "svelte", label: `${emoji.get("orange_heart")} Svelte` },
     // { value: "qwik", label: `${emoji.get("globe_with_meridians")} Qwik` },
   ];
 
-  const linterOptions = [
-    { value: "eslint", label: `${emoji.get("white_check_mark")} ESLint` },
-    // { value: "tslint", label: `${emoji.get("no_entry")} TSLint (deprecated)` },
-    { value: "prettier", label: `${emoji.get("nail_care")} Prettier` },
-    // { value: "standard", label: `${emoji.get("barber")} Standard` },
-    // { value: "airbnb", label: `${emoji.get("house_with_garden")} Airbnb` },
-    // { value: "google", label: `${emoji.get("earth_americas")} Google` },
-    { value: "none", label: `${emoji.get("no_entry_sign")} None` },
-  ];
+  // const linterOptions = [
+  //   { value: "eslint", label: `${emoji.get("white_check_mark")} ESLint` },
+  //   // { value: "tslint", label: `${emoji.get("no_entry")} TSLint (deprecated)` },
+  //   { value: "prettier", label: `${emoji.get("nail_care")} Prettier` },
+  //   // { value: "standard", label: `${emoji.get("barber")} Standard` },
+  //   // { value: "airbnb", label: `${emoji.get("house_with_garden")} Airbnb` },
+  //   // { value: "google", label: `${emoji.get("earth_americas")} Google` },
+  //   // { value: "none", label: `${emoji.get("no_entry_sign")} None` },
+  // ];
 
   intro(
     `${emoji.get(
@@ -109,10 +398,13 @@ async function main() {
   var install_command = `npm create vite@latest ${projectName} -- --template ${framework}`;
   console.log(install_command);
 
-  const linter = await select({
-    message: `${emoji.get("gear")} Select a linter`,
-    options: linterOptions,
-  });
+  // const linter = await multiselect({
+  //   message: `${emoji.get(
+  //     "gear"
+  //   )} What linter do you want to use? (Select multiple) or select None`,
+  //   options: linterOptions,
+  //   required: false,
+  // });
 
   const needTailwind = await confirm({
     message: `${emoji.get("thought_balloon")} Do you need Tailwind CSS?`,
@@ -151,7 +443,7 @@ async function main() {
       console.error(chalk.red(stderr));
       process.exit(1);
     } else {
-      console.log(chalk.gray(stdout));
+      console.log(chalk.bgBlue("running"), chalk.gray(stdout));
       process.chdir(projectPath);
       exec("npm install", (error, stdout, stderr) => {
         spin.start(
@@ -165,87 +457,19 @@ async function main() {
           console.error(chalk.red(stderr));
           process.exit(1);
         } else {
-          console.log(chalk.gray(stdout));
-          const linterCommand = linter === "none" ? "" : `npx ${linter} --init`;
-
-          //! eslint-config-standard
-          //! 1npm init @eslint/config@latest -- --config eslint-config-standard
-
-          //for prettier
-          // npm install --save-dev --save-exact prettier
-          // Then, create an empty config file to let editors and other tools know you are using Prettier:
-          // node --eval "fs.writeFileSync('.prettierrc','{}\n')"
-
-          // complete the linter setup
-          // if (linter !== "none") {
-          //   exec(linterCommand, (error, stdout, stderr) => {
-          //     if (error) {
-          //       spin.stop(`${emoji.get("x")} Failed to initialize linter.`);
-          //       console.error(chalk.red(stderr));
-          //       process.exit(1);
-          //     } else {
-          //       console.log(chalk.gray(stdout));
-          //       spin.stop(
-          //         chalk.bgGreenBright(
-          //           chalk.bgGreenBright("Project created successfully! 🚀")
-          //         )
-          //       );
-          //       process.exit(0);
-          //     }
-          //   });
-          // }
-
+          console.log(chalk.bgBlue("running"), chalk.gray(stdout));
+          addStubCode(projectPath, framework, projectName);
           if (!needTailwind) {
-            spin.stop(
-              chalk.bgGreenBright(
-                chalk.bgGreenBright("Project created successfully! 🚀")
-              )
-            );
+            spin.stop(chalk.bgGreenBright("Project created successfully! 🚀"));
             process.exit(0);
           } else {
-            exec(
-              "npm install -D tailwindcss@latest postcss@latest autoprefixer@latest",
-              (error, stdout, stderr) => {
-                spin.start(
-                  chalk.bgYellow(
-                    `${emoji.get("hammer_and_wrench")} Installing Tailwind CSS`
-                  )
-                );
-                console.log(
-                  chalk.blueBright(
-                    "npm install -D tailwindcss postcss autoprefixer"
-                  )
-                );
-                if (error) {
-                  spin.stop(
-                    `${emoji.get("x")} Failed to install Tailwind CSS.`
-                  );
-                  console.error(chalk.red(stderr));
-                  process.exit(1);
-                } else {
-                  console.log(chalk.gray(stdout));
-                }
-              }
-            );
+            installTailwind(framework, projectPath, projectName, spin);
+            spin.stop(chalk.bgGreenBright("Project created successfully! 🚀"));
           }
         }
       });
     }
   });
-
-  // setTimeout(() => {
-  //   spin.stop(
-  //     chalk.bgGreenBright(chalk.blackBright("Project created successfully! 🚀"))
-  //   );
-  // }, 2000);
-  // console.log(
-  //   chalk.bgRedBright(chalk.white("  Selected  ")),
-  //   chalk.yellow(`Tailwind CSS: ${needTailwind}`)
-  // );
-
-  // npm create vite@latest my-vue-app -- --template vue --typescript
-  // npm create vite@latest my-react-app -- --template react-ts
-  // this is the command
 }
 
 displayHeader();
